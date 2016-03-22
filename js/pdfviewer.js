384215ba19f403b05dfb408a8100aceaccdd798a
srgandawali <srgandawali@docudavit.com>
2016-03-22 15:47:01 -0400

    Init

---
+++ js/pdfviewer.js
@@ -0,0 +1,16 @@
+//  pdfviewer.js
+//
+//  Created by Pierre-Emmanuel Bois on 08/08/13.
+//
+//  Copyright 2012-2013 Pierre-Emmanuel Bois. All rights reserved.
+//  MIT Licensed
+
+var PDFViewer = {
+    open: function (url, title, callback) {
+        cordova.exec(callback, function (err) {
+            callback('Error: Preview');
+        }, "PDFViewer", "open", [url, title]);
+    }
+};
+
+module.exports = PDFViewer;
